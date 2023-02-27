8649007f7c1020097327576c708a61184e6194be
poruruba <44470781+poruruba@users.noreply.github.com>
2023-02-27 21:00:20 +0900

    Update index.js

--- api/controllers/yamareco-api/index.js
+++ api/controllers/yamareco-api/index.js
@@ -12,75 +12,75 @@ const fetch = require('node-fetch');
 const Headers = fetch.Headers;
 
 exports.handler = async (event, context, callback) => {
-	if( event.path == '/yamareco-get-list' ){
-    var body = JSON.parse(event.body);
-    console.log(body);
-  
-    var counter = 1;
-    var userId = body.userId;
-		var response = await do_get(base_url + "/getReclist/user/" + userId + '/' + counter++);
-    console.log(response);
-    if( response.err != 0 )
-      throw response.errcode;
-    var reclist = response.reclist;
-    try{
-      while(true){
+    if (event.path == '/yamareco-get-list') {
+        var body = JSON.parse(event.body);
+        console.log(body);
+
+        var counter = 1;
+        var userId = body.userId;
         var response = await do_get(base_url + "/getReclist/user/" + userId + '/' + counter++);
         console.log(response);
-        if( response.err != 0 )
-          throw response.errcode;
-        reclist = reclist.concat(response.reclist);
-      }
-    }catch(error){}
-		return new Response(reclist);
-  }else
-	if( event.path == '/yamareco-get-user' ){
-    var body = JSON.parse(event.body);
-    console.log(body);
-  
-    var userId = body.userId;
-		var response = await do_get(base_url + "/getUserInfo/" + userId);
-    console.log(response);
-    if( response.err != 0 )
-      throw response.errcode;
-		return new Response(response.userinfo);
-	}else
-	if( event.path == '/yamareco-get-gpx' ){
-    var qs = event.queryStringParameters;
-    console.log(qs);
-		var recId = qs.recId;
-		var response = await do_get_text(base_url_gpx + "track-" + recId + '.gpx');
-		return new TextResponse("application/gpx+xml", response);
-	}
+        if (response.err != 0)
+            throw response.errcode;
+        var reclist = response.reclist;
+        try {
+            while (true) {
+                var response = await do_get(base_url + "/getReclist/user/" + userId + '/' + counter++);
+                console.log(response);
+                if (response.err != 0)
+                    throw response.errcode;
+                reclist = reclist.concat(response.reclist);
+            }
+        } catch (error) {}
+        return new Response(reclist);
+    } else
+    if (event.path == '/yamareco-get-user') {
+        var body = JSON.parse(event.body);
+        console.log(body);
+
+        var userId = body.userId;
+        var response = await do_get(base_url + "/getUserInfo/" + userId);
+        console.log(response);
+        if (response.err != 0)
+            throw response.errcode;
+        return new Response(response.userinfo);
+    } else
+    if (event.path == '/yamareco-get-gpx') {
+        var qs = event.queryStringParameters;
+        console.log(qs);
+        var recId = qs.recId;
+        var response = await do_get_text(base_url_gpx + "track-" + recId + '.gpx');
+        return new TextResponse("application/gpx+xml", response);
+    }
 };
 
 function do_get(url, qs) {
-  console.log(url);
-  var params = new URLSearchParams(qs);
+    console.log(url);
+    var params = new URLSearchParams(qs);
 
-  var params_str = params.toString();
-  var postfix = (params_str == "") ? "" : ((url.indexOf('?') >= 0) ? ('&' + params_str) : ('?' + params_str));
-  return fetch(url + postfix, {
-      method: 'GET',
-    })
-    .then((response) => {
-      if (!response.ok)
-        throw new Error('status is not 200');
-      return response.json();
-    });
+    var params_str = params.toString();
+    var postfix = (params_str == "") ? "" : ((url.indexOf('?') >= 0) ? ('&' + params_str) : ('?' + params_str));
+    return fetch(url + postfix, {
+            method: 'GET',
+        })
+        .then((response) => {
+            if (!response.ok)
+                throw new Error('status is not 200');
+            return response.json();
+        });
 }
 
 function do_get_text(url, qs) {
-  var params = new URLSearchParams(qs);
+    var params = new URLSearchParams(qs);
 
-  var params_str = params.toString();
-  var postfix = (params_str == "") ? "" : ((url.indexOf('?') >= 0) ? ('&' + params_str) : ('?' + params_str));
-  return fetch(url + postfix, {
-      method: 'GET',
-    })
-    .then((response) => {
-      if (!response.ok)
-        throw new Error('status is not 200');
-      return response.text();
-    });
+    var params_str = params.toString();
+    var postfix = (params_str == "") ? "" : ((url.indexOf('?') >= 0) ? ('&' + params_str) : ('?' + params_str));
+    return fetch(url + postfix, {
+            method: 'GET',
+        })
+        .then((response) => {
+            if (!response.ok)
+                throw new Error('status is not 200');
+            return response.text();
+        });
 }
